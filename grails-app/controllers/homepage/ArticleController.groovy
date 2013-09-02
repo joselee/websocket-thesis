3850276ba4eb87128d7c8a67c17351934c22a912
Jose Lee <jose.lee@conmio.com>
2013-09-02 15:32:26 +0300

    ArticleController gets dummy json file and renders it for now

--- grails-app/controllers/homepage/ArticleController.groovy
+++ grails-app/controllers/homepage/ArticleController.groovy
@@ -1,10 +1,17 @@
 package homepage
 
+import grails.converters.*
+
 class ArticleController {
+
+    def grailsApplication
 
     def index() { }
 
     def articles = {
 
+        File jsonFile = grailsApplication.parentContext.getResource("feeds/articles.json").file
+        def articleFeed = JSON.parse(jsonFile.text)
+        render articleFeed as JSON
     }
 }
