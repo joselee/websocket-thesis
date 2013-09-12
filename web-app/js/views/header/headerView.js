ff131a88383658de8d34a9b9b20666135a3b200a
Jose Lee <jose.lee@live.com>
2013-09-13 00:10:13 +0300

    Mobile header menu toggles closed when an item is clicked

--- web-app/js/views/header/headerView.js
+++ web-app/js/views/header/headerView.js
@@ -8,11 +8,21 @@ define(
             className: "headerView",
 			template: headerViewTemplate,
 			events: {
-				"click .mobileMenuButton": "toggleMobileMenu"
+				"click .mobileMenuButton": "toggleMobileMenu",
+                "click .headerLinkListItem": "mobileMenuItemSelected",
+                "click .homeLogo": "mobileMenuItemSelected"
 			},
 			toggleMobileMenu: function(){
 				$(".headerLinksContainer", this.$el).slideToggle("150");
-			}
+			},
+            mobileMenuItemSelected: function(){
+                var mobile = $(".mobileMenuButton").css("display") === "block";
+                var menuIsOpen = $(".headerLinksContainer").not(":hidden");
+
+                if(mobile && menuIsOpen){
+                    $(".headerLinksContainer", this.$el).slideUp("150");
+                }
+            }
 		});
 
         return HeaderView;
